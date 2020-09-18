0a4e00904a8fd4ca222cf95d9ea8f6873746ed93
Ernie <jasonernie03@gmail.com>
2020-09-18 19:55:20 -0400

    add gravity and space jump

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -9,10 +9,27 @@ public class PlayerMovementScript : MonoBehaviour
     
     public CharacterController gameProtagonist;    // 游戏主角
 
-    
+    public Transform goundCheck;
+    public float groundDistance = 0.4f; // radius of sphere, the sphere of that empty object to detect whether we are touching the ground
+    public LayerMask groundMask;
+    bool isGrounded;
+
+    Vector3 velocity; // verify this oart again 
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(goundCheck.position, groundDistance, groundMask);                      // return true if any colliders overlapping , check the position of groundMask and groundMask, if their distance less than groundDistance, then return true
+        if (isGrounded && velocity.y < 0) {
+            velocity.y = -2f;
+        }
+
+
+
+
         float x = Input.GetAxis("Horizontal");  // controller pour à gauche et à droit
         float z = Input.GetAxis("Vertical");
         // below is move direciton
@@ -20,6 +37,13 @@ public class PlayerMovementScript : MonoBehaviour
 
 
         gameProtagonist.Move(move / 10 );
+
+        velocity.y += gravity * 1.8f * Time.deltaTime;
+        gameProtagonist.Move(velocity * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+        }
     }
     
     
