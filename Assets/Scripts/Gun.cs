e6b4d2cc417f44a83dc35758bec80e0a0bb2ff3c
Ernie <jasonernie03@gmail.com>
2020-09-24 11:10:00 -0400

    gun shots sound and bgm

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class Gun : MonoBehaviour
 {
     public GameObject bullet;
+    public AudioSource gunShotsSound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gunShotsSound = GetComponent<AudioSource>();   
     }
 
     // Update is called once per frame
@@ -22,6 +24,7 @@ public class Gun : MonoBehaviour
             newBullet.transform.position = transform.position + transform.up; // when instantiate let the bullet appear right in front of the gun
             newBullet.GetComponent<Rigidbody>().velocity = transform.up * 25 ;
             newBullet.SetActive(true);
+            gunShotsSound.Play();
         }
         
     }
