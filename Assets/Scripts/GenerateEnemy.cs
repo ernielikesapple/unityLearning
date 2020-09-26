e99b8f422200baa862f511d597a7d5e8c5a6f6a1
Ernie <jasonernie03@gmail.com>
2020-09-25 22:29:22 -0400

    enemy can randomly spawn

---
+++ Assets/Scripts/GenerateEnemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateEnemy : MonoBehaviour
+{
+    public GameObject theEnemy;
+    public int spawnXMaxPos = 20;
+    public int spawnZMaxPos = 20;
+    public int enemyMaxCount = 50;
+    private int xPos;
+    private int zPos;
+    private int enemyCount;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemyDrop());
+    }
+
+    IEnumerator EnemyDrop()
+    {
+        while (enemyCount < enemyMaxCount)
+        {
+            xPos = (Random.Range(-spawnXMaxPos, spawnXMaxPos));
+            zPos = (Random.Range(-spawnZMaxPos, spawnZMaxPos));
+            Instantiate(theEnemy, new Vector3(xPos, 0, zPos), Quaternion.identity);
+            yield return new WaitForSeconds(0.1f);    // 1 second to put 10 enemies
+            enemyCount += 1;
+        }
+
+
+    }
+    
+}
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,12 +7,14 @@ public class Gun : MonoBehaviour
 
     public float damage =  10f; // use the Raycasts shooting mechanism
     public float range = 100f; // change this var to adjust how far the gun can hit
-
+    // public float fireRate = 15f;  // for shotting mutiple times
+    public float impactForce = 30f;
 
     public Camera fpsCamera;  // shoot a ray starting at the position of the camera
     public ParticleSystem muzzelFlash;  // the explosive effect in the gun
     public GameObject impactEffect; // the gun effect when the gun hit sth
 
+    // private float nextTimeToFire = 0f; //  for shotting mutiple times
 
     // Start is called before the first frame update
     void Start()
@@ -23,25 +25,26 @@ public class Gun : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //if (Input.GetMouseButton(0) && Time.time >= nextTimeToFire)  for shotting mutiple times
         if (Input.GetMouseButtonDown(0))
         {
-            GameObject newBullet = ObjectPool.SharedInstance.GetPooledObject(); // pre initialized bullets from ObjectPool.cs
-            newBullet.transform.position = transform.position + transform.up; // when instantiate let the bullet appear right in front of the gun
-            newBullet.GetComponent<Rigidbody>().velocity = transform.up * 40 ;
-            newBullet.SetActive(true);
-            gunShotsSound.Play();
-
+            // nextTimeToFire = Time.time + 1f / fireRate;   //  for shotting mutiple times
             Shoot();
-
-
         }
         
     }
 
     void Shoot() {
 
-        muzzelFlash.Play();
+        // bullet sections
+        GameObject newBullet = ObjectPool.SharedInstance.GetPooledObject(); // pre initialized bullets from ObjectPool.cs
+        newBullet.transform.position = transform.position + transform.up; // when instantiate let the bullet appear right in front of the gun
+        newBullet.GetComponent<Rigidbody>().velocity = transform.up * 40;
+        newBullet.SetActive(true);
+        gunShotsSound.Play();
 
+
+        muzzelFlash.Play();
 
         RaycastHit hit;  // var to hold the info about what we hit
 
@@ -50,9 +53,13 @@ public class Gun : MonoBehaviour
             // return true if we hit sth
             Enemy enemy = hit.transform.GetComponent<Enemy>();
             if (enemy != null) {
-                Debug.Log("?????????????");
                 enemy.TakeDamage(damage);
 
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
             }
 
             Debug.Log(hit.transform.name);
